3a94f60f65fb25db3a9f0e6d99adfc2143d1d0b1
Andrey Yurzanov <yurzanov.andrey@gmail.com>
2019-09-29 23:50:17 +1000

    .npmignore

--- src/index.js
+++ src/index.js
@@ -1,33 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import { Tree, defConf } from './components/tree';
-import 'babel-polyfill';
+import { single, multi } from './components/expand';
+import { param, field } from './components/resolve';
 
-const children = [
-  {
-    title: 'Item-1',
-    children: [
-      {
-        title: 'Subitem-1-1'
-      },
-      {
-        title: 'Subitem-1-2'
-      }        
-    ]
-  },
-  {
-    title: 'Item-2',
-    children: [
-      {
-        title: 'Subitem-2-1'
-      },
-      {
-        title: 'Subitem-2-2'
-      }         
-    ]
-  }
-];
-const conf = defConf(children);
-
-const root = document.getElementById('root');
-ReactDOM.render((<Tree conf={ conf } />), root);
+export { Tree, defConf, single, multi, param, field };
